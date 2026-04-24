11b7ec53399b893d95dcc874e3faa68923801afb
Anton Levin <64654309+holeinbrain@users.noreply.github.com>
2026-04-24 20:17:47 +0200

    product-template.js

--- product-template.js
+++ product-template.js
@@ -2,8 +2,7 @@
   function hidePlaceholders() {
     document.querySelectorAll('.feature-card').forEach(function (el) {
       var text = el.textContent
-        .replace('✔', '')
-        .replace(/\u00a0/g, '') // убирает &nbsp;
+        .replace(/[^\w\u0400-\u04FF]/g, '') // убирает всё кроме букв (латиница + кириллица)
         .trim();
       if (!text || text.startsWith('#?')) {
         el.style.display = 'none';
@@ -12,7 +11,7 @@
 
     document.querySelectorAll('.product-bullets li').forEach(function (el) {
       var text = el.textContent
-        .replace(/\u00a0/g, '')
+        .replace(/[^\w\u0400-\u04FF]/g, '')
         .trim();
       if (!text || text.startsWith('#?')) {
         el.style.display = 'none';
